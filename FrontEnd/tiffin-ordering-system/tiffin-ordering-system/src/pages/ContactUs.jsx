1efa2757b52d367a3afe1427818568a713529f4f
@67633_Mrunali <mrunalidhote14102000@gmail.com>
2024-02-28 19:52:17 +0530

    frontend

---
+++ FrontEnd/tiffin-ordering-system/tiffin-ordering-system/src/pages/ContactUs.jsx
@@ -0,0 +1,23 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Have a question, feedback, or need assistance with your order? We're here to help! Reach out to us through any of the following channels:
+        <br />
+        <br />
+        - Phone: 9834618264
+        <br />
+        - Email: mrunalidhote14102000@gmail.com
+        <br />
+        - Address: Chandrapur, Maharashtra
+        <br />
+        
+
+    Our dedicated team is ready to assist you with any inquiries or concerns. We look forward to hearing from you!
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
